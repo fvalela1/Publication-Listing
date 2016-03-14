6395582a02382b4e2ac77a42351b6a13d6e240da
Francesco Valela <valela.francesco@gmail.com>
2016-03-14 18:42:44 -0400

    Base class which simply holds a constructor

---
+++ Publication.java
@@ -0,0 +1,27 @@
+// ------------------------------------------------------------------------------- 
+// Assignment 2
+// Questions: Part 1 & 2
+// Written by: Francesco Valela 26669883
+// For COMP249 Section: S
+// ------------------------------------------------------------------------------
+// A class that allows the other classes to create Publication objects
+
+import java.io.Serializable;
+
+public class Publication implements Serializable{
+	long publication_code;
+	String publication_name;
+	int publication_year;
+	String publication_authorname;
+	double publication_cost;
+	int publication_nbpages;
+	
+	public Publication(long pCode, String pName, int pYear, String pAuthorName, double pCost, int pPages) {
+		publication_code = pCode;
+		publication_name = pName;
+		publication_year = pYear;
+		publication_authorname = pAuthorName;
+		publication_cost = pCost;
+		publication_nbpages = pPages;
+	}
+}
